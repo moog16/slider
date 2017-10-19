b98f56c8a56d93dc8de1fda3084b8073454a6823
Matt Goo <matthew.goo@points.com>
2017-10-19 11:41:57 -0700

    upgraded tooltip package

--- src/createSliderWithTooltip.jsx
+++ src/createSliderWithTooltip.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Tooltip from 'rc-tooltip';
+import Tooltip from 'rc-tooltip-es6';
 import Handle from './Handle';
 
 export default function createSliderWithTooltip(Component) {
